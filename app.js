c7215c54b646a2b5527d5ce706e580f6f6e0332a
luwat <oluwatobioreniyi@gmail.com>
2023-03-20 19:25:18 +0100

    testing in the console

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-let myLibrary = [];
+/* eslint-disable no-unused-vars */
+const myLibrary = [];
 
+// eslint-disable-next-line no-unused-vars
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
@@ -7,6 +9,18 @@ function Book(title, author, pages, read) {
     this.read = read
 }
 
-function addBookToLibrary() {
-
+Book.prototype.sayTitle = function () {
+    return `The title of this book is ${this.title}.`
 }
+
+
+const money = new Book('Money', 'Bob Rick', '186', 'not yet read');
+const theDignityOfManHood = new Book('The Dignity of Manhood', 'Gbile Akanni', '158', 'read')
+console.log(money.sayTitle())
+console.log(money)
+myLibrary.push(money, theDignityOfManHood)
+console.log(myLibrary)
+
+// function addBookToLibrary() {
+
+// }
