a8247edd538ae9b9b796e6bf8640100f72d9a8a5
luwat <oluwatobioreniyi@gmail.com>
2023-03-23 11:45:48 +0100

    trying arrays on dom

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bookAuthor = document.querySelector('.author')
 const bookPages = document.querySelector('.pages')
 const bookRead = document.querySelector('.read')
 const addBook = document.querySelector('.submit')
-
+const cards = document.querySelector('.cards')
 // eslint-disable-next-line no-unused-vars
 function Book(title, author, pages, read) {
   this.title = title;
@@ -17,10 +17,25 @@ function Book(title, author, pages, read) {
 
 
 function addBookToLibrary(e) {
-    e.preventDefault()
-    libraryBooks = new Book(bookTitle.value, bookAuthor.value, bookPages.value, bookRead.value);
-    console.log(myLibrary);
-  return myLibrary.push(libraryBooks);
+  e.preventDefault()
+  if (bookTitle.value === '' ||
+      bookAuthor.value === '' ||
+      bookPages.value === '' ||
+      bookRead.value === '') {
+        alert('insert data')
+      } else {
+        libraryBooks = new Book(bookTitle.value, bookAuthor.value, bookPages.value, bookRead.value);
+        console.log(myLibrary);
+        return myLibrary.push(libraryBooks);
+      }
 }
 
 addBook.addEventListener('click', addBookToLibrary)
+addBook.addEventListener('click', () => {
+  const books = myLibrary.map((book) => {
+    cards.textContent = book;
+    return cards;
+  })
+
+  return books
+})
