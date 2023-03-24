43fa28ce5ed9d608ff4590fd3606ad0af0eaa3d7
luwat <oluwatobioreniyi@gmail.com>
2023-03-24 17:01:47 +0100

    add array to DOM

--- app.js
+++ app.js
@@ -20,11 +20,10 @@ function addBookToLibrary(e) {
   e.preventDefault()
   if (bookTitle.value === '' ||
       bookAuthor.value === '' ||
-      bookPages.value === '' ||
-      bookRead.value === '') {
+      bookPages.value === '' ) {
         alert('insert data')
       } else {
-        libraryBooks = new Book(bookTitle.value, bookAuthor.value, bookPages.value, bookRead.value);
+        libraryBooks = new Book(bookTitle.value, bookAuthor.value, bookPages.value, bookRead.checked);
         console.log(myLibrary);
         return myLibrary.push(libraryBooks);
       }
@@ -32,9 +31,30 @@ function addBookToLibrary(e) {
 
 addBook.addEventListener('click', addBookToLibrary)
 addBook.addEventListener('click', () => {
-  const books = myLibrary.map((book) => {
-    cards.textContent = book;
-    return cards;
+  const card = document.createElement('div');
+  card.setAttribute('class', 'card');
+  const cardTitle = document.createElement('p');
+  cardTitle.setAttribute('class', 'card-title');
+  const cardAuthor = document.createElement('p');
+  cardAuthor.setAttribute('class', 'card-author');
+  const cardPages = document.createElement('p');
+ cardPages.setAttribute('class', 'card-pages');
+  const cardRead = document.createElement('p');
+ cardRead.setAttribute('class', 'card-read');
+  const isRead = document.createElement('button');
+  isRead.setAttribute('class', 'btn-read');
+  card.appendChild(cardTitle)
+  card.appendChild(cardAuthor)
+  card.appendChild(cardPages)
+  card.appendChild(cardRead)
+  card.appendChild(isRead)
+  const books = myLibrary.map(({title, author, pages, read}) => {
+    cardTitle.textContent = `Book title: ${title}`
+    cardAuthor.textContent = `Author: ${author}`
+    cardPages.textContent = `Pages: ${pages}`
+    cardRead.textContent = `I have ${read}`
+
+    return cards.appendChild(card);
   })
 
   return books
